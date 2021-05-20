e80394bb5bf6264017448d755f7a2ad4561afcc7
Marcel <maschmidt.dev@gmail.com>
2021-05-21 01:42:31 +0200

    restored private route

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
         <AuthWrapper>
             <Router>
                 <Switch>
-                    <Route path='/' exact={true}>
+                    <PrivateRoute path='/' exact={true}>
                         <Dashboard></Dashboard>
-                    </Route>
+                    </PrivateRoute>
                     <Route path='/login'>
                         <Login />
                     </Route>
